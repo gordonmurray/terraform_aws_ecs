f3b318135d0f0abcc8ffcf7b851d3f2308500ef1
Gordon Murray <gordon@gordonmurray.com>
2024-07-22 19:04:31 +0100

    fix route

--- route_table.tf
+++ route_table.tf
@@ -9,11 +9,6 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
-
   tags = {
     Name = "private-route-table"
   }
